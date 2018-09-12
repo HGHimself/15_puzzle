dd3ba8e4d5aa631a76206eac147cf1c68dd5eb53
MosesHimself <hmkwp5@mail.missouri.edu>
2018-09-12 13:55:30 -0500

    depth first breaks tie by choosing smaller number

--- Searcher.rb
+++ Searcher.rb
@@ -90,31 +90,55 @@ class Searcher
   def successor_function(node)
 
     newStates = Array.new
+    values = Array.new
+    #here we gotta check each possible move for this node
+    #node.direction returns a copy of the state after a move in said direction
+    #false if move isnt possible
 
-    down = node.down
-    if down != false
-      #puts "down works!"
-      newStates.push(down)
+    up = node.upValue
+    if up != nil
+      #puts "Up works!"
+      upState = node.up
+      newStates.push(upState)
+      values.push(up)
     end
 
-    right = node.right
-    if right != false
+    down = node.downValue
+    if down != nil
       #puts "down works!"
-      newStates.push(right)
+      downState = node.down
+      newStates.push(downState)
+      values.push(down)
     end
 
-    up = node.up
-    if up != false
-      #puts "down works!"
-      newStates.push(up)
+    left = node.leftValue
+    if left != nil
+      #puts "left works!"
+      leftState = node.left
+      newStates.push(leftState)
+      values.push(left)
     end
 
-    left = node.left
-    if left != false
-      #puts "down works!"
-      newStates.push(left)
+    right = node.rightValue
+    if right != nil
+      #puts "right works!"
+      rightState = node.right
+      newStates.push(rightState)
+      values.push(right)
     end
 
+    swapped = true
+    while swapped do
+      swapped = false
+      0.upto(values.size-2) do |i|
+        if values[i] < values[i+1]
+          values[i], values[i+1] = values[i+1], values[i] # swap values
+          newStates[i], newStates[i+1] = newStates[i+1], newStates[i] # swap values
+          swapped = true
+        end
+      end
+    end
+    
     return newStates
 
   end
--- test.rb
+++ test.rb
@@ -1,52 +1,5 @@
 
 
 =begin
-values = Array.new
-#here we gotta check each possible move for this node
-#node.direction returns a copy of the state after a move in said direction
-#false if move isnt possible
 
-up = node.upValue
-if up != nil
-  #puts "Up works!"
-  upState = node.up
-  newStates.push(upState)
-  values.push(up)
-end
-
-down = node.downValue
-if down != nil
-  #puts "down works!"
-  downState = node.down
-  newStates.push(downState)
-  values.push(down)
-end
-
-left = node.leftValue
-if left != nil
-  #puts "left works!"
-  leftState = node.left
-  newStates.push(leftState)
-  values.push(left)
-end
-
-right = node.rightValue
-if right != nil
-  #puts "right works!"
-  rightState = node.right
-  newStates.push(rightState)
-  values.push(right)
-end
-
-swapped = true
-while swapped do
-  swapped = false
-  0.upto(values.size-2) do |i|
-    if values[i] < values[i+1]
-      values[i], values[i+1] = values[i+1], values[i] # swap values
-      newStates[i], newStates[i+1] = newStates[i+1], newStates[i] # swap values
-      swapped = true
-    end
-  end
-end
 =end
