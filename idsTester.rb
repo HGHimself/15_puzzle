872456f7d43f97a5e8d1d26a7a15b0b42c384138
Olivia Ledford <dev.oledford@gmail.com>
2018-09-13 16:03:53 -0500

    conventions

--- idsTester.rb
+++ idsTester.rb
@@ -1,14 +1,14 @@
 require_relative 'puzzleNode'
 require_relative 'ids'
 
-goal_1   = ARGV[1] # or set this to a string with your own goal
-goal_val = PuzzleNode.parse_state(goal_1, true)
+goal_input   = ARGV[1] # or set this to a string with your own goal
+goal_val = PuzzleNode.parse_state(goal_input, true)
 
-start_string = ARGV[0] #or set this to one of the difficulties defined above
+initial_string = ARGV[0] #or set this to one of the difficulties defined above
 
-s0                   = PuzzleNode.parse_state(start_string)
+s0                   = PuzzleNode.parse_state(initial_string)
 s0.goal_value        = goal_val
-goal_node            = PuzzleNode.parse_state(goal_1)
+goal_node            = PuzzleNode.parse_state(goal_input)
 goal_node.goal_value = goal_val
 
 def print_helper(array)
@@ -22,14 +22,14 @@ end
 
 puts ' '
 puts '------- ITERATIVE DEEPENING DEPTH LIMITED SEARCH (Start Depth = 4, Inc = 1) --------'
-searcher = DepthLimitedSearcher.new(s0, goal_node, :generate_states, 4, 1)
-start = Time.now
-solution = searcher.search
+searchStrategy = DepthLimitedSearcher.new(s0, goal_node, :generate_states, 4, 1)
+startTime = Time.now
+solution = searchStrategy.search
 puts "| Solution: #{solution}"
-puts "| Number of nodes visted: #{searcher.num_nodes_visited}"
-puts "| Size of open list: #{searcher.open.size}"
-puts "| Number of nodes that were already generated: #{searcher.already_generated}"
-puts "| Time taken: #{Time.now - start}s"
+puts "| Number of nodes visted: #{searchStrategy.num_nodes_visited}"
+puts "| Size of open list: #{searchStrategy.open.size}"
+puts "| Number of nodes that were already generated: #{searchStrategy.already_generated}"
+puts "| Time taken: #{Time.now - startTime}s"
 puts '-------------------------------------------------------------------------------------'
 
 
