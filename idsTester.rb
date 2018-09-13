e17f74542f1401a0f42df652848fe5d44553355f
Olivia Ledford <dev.oledford@gmail.com>
2018-09-13 16:45:13 -0500

    Edited parsing algorithm

--- idsTester.rb
+++ idsTester.rb
@@ -1,18 +1,20 @@
 require_relative 'puzzleNode'
 require_relative 'ids'
 
+# Reads in the goal state from the command line and parses it for the print statement.
 goal_input   = ARGV[1]
-goal_val = PuzzleNode.parse_state(goal_input, true)
+goal_val = PuzzleNode.parseState(goal_input, true)
 
+# Reads in the initial state from the command line and parses it
 start_input = ARGV[0]
-start_val = PuzzleNode.parse_state(start_input, true)
+start_val = PuzzleNode.parseState(start_input, true)
 
-start_node                   = PuzzleNode.parse_state(start_input)
+start_node                   = PuzzleNode.parseState(start_input)
 start_node.goal_value        = goal_val
-goal_node            = PuzzleNode.parse_state(goal_input)
+goal_node            = PuzzleNode.parseState(goal_input)
 goal_node.goal_value = goal_val
 
-def print_helper(array)
+def printHelper(array)
   array.each do |sub_array|
     sub_array.each do |tile|
       if tile < 10
@@ -27,18 +29,18 @@ end
 
 puts ' '
 puts '      Start:'
-print_helper(start_val)
+printHelper(start_val)
 puts ' '
 
 puts '      Goal:'
-print_helper(goal_val)
+printHelper(goal_val)
 puts ' '
 
 puts 'Note that the 0 displayed in the above arrays is indicative of the empty tile in the 15 Puzzle.'
 puts ''
 
 puts '------- ITERATIVE DEEPENING DEPTH LIMITED SEARCH (Start Depth = 4, Increment = 1) --------'
-searchStrategy = DepthLimitedSearcher.new(start_node, goal_node, :generate_states, 4, 1)
+searchStrategy = DepthLimitedSearcher.new(start_node, goal_node, :generateStates, 4, 1)
 startTime = Time.now
 solution = searchStrategy.searcher
 puts "| Solution: #{solution}"
--- puzzleNode.rb
+++ puzzleNode.rb
@@ -1,21 +1,20 @@
 class PuzzleNode
   attr_reader :value, :moves
-  attr_accessor :g_hat, :heuristic, :goal_value
+  attr_accessor :goal_value, :heuristic
 
-  def initialize(value, blank, goal_value, heuristic = :manhattan, moves = '')
+  def initialize(value, blank, goal_value, heuristic, moves = '')
     @goal_value = goal_value
     @heuristic = heuristic
     @moves = moves
     @value = value
     @blank = blank
-    @g_hat = 1
   end
 
-  def self.parse_state state, value_only = false
+  def self.parseState state, value_only = false
     # Yay for Ruby one liners
-    ary = state.split(/[ \(\)]+/).reject{|e| e == ""}.map(&:to_i)
-    squares = ary[0..-3].each_slice(4).to_a
-    blank = ary[-2..-1]
+    array = state.split(/[ \(\)]+/).reject{|e| e == ""}.map(&:to_i)
+    squares = array[0..-3].each_slice(4).to_a
+    blank = array[-2..-1]
     if value_only
       squares
     else
@@ -23,7 +22,7 @@ class PuzzleNode
     end
   end
 
-  def generate_states
+  def generateStates
     states = [ ]
     states << west  if west?
     states << north if north?
@@ -32,38 +31,14 @@ class PuzzleNode
     states.shuffle
   end
 
-  def h_hat
-    # manhattan distance. Ya, this is ugly
-    manhattan_sum = 0
-    @value.each_with_index do |row_ary, row_i|
-      row_ary.each_with_index do |col_el, col_i|
-        row, col = find_position_in_goal(col_el)
-        distance = (row_i - row).abs + (col_i - col).abs
-        if @heuristic != :manhattan
-          range_of_vals = @goal_value[row_i]#(row_i*4)..(row_i*4+3)
-          i = -1
-          if range_of_vals.include?(col_el) and
-            range_of_vals.any? { |e|
-              i += 1
-              (e < col_el and col_i < i) or (e > col_el and col_i > i)
-            }
-              distance += 1
-          end
-        end
-        manhattan_sum += distance
-      end
-    end
-    manhattan_sum
-  end
-
-  def find_position_in_goal num
+  def findPositionInGoal_position_in_goal num
     goal = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
     row = goal.index { |row| row.include?(num) }
     col = goal[row].index(num)
     return row, col
   end
 
-  def move_blank_to to_row, to_col, move
+  def moveBlankTo to_row, to_col, move
     val  = Marshal.load(Marshal.dump(@value))
     row_1, col_1 = @blank
     blnk = [to_row, to_col]
@@ -77,28 +52,28 @@ class PuzzleNode
   end
   def north
     row, col = @blank
-    move_blank_to(row - 1, col, 'N')
+    moveBlankTo(row - 1, col, 'N')
   end
   def south?
     @blank[0] < 3 and @moves[-1] != 'N'
   end
   def south
     row, col = @blank
-    move_blank_to(row + 1, col, 'S')
+    moveBlankTo(row + 1, col, 'S')
   end
   def east?
     @blank[1] < 3 and @moves[-1] != 'W'
   end
   def east
     row, col = @blank
-    move_blank_to(row, col + 1, 'E')
+    moveBlankTo(row, col + 1, 'E')
   end
   def west?
     @blank[1] > 0 and @moves[-1] != 'E'
   end
   def west
     row, col = @blank
-    move_blank_to(row, col - 1, 'W')
+    moveBlankTo(row, col - 1, 'W')
   end
   def to_s
     @moves
